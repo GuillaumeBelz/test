74d5361561c74a38a96a5655e090cd5942b695fa
Guillaume Belz <1651508+GuillaumeBelz@users.noreply.github.com>
2023-04-22 17:27:10 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <stdlib.h>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+  char *x = (char*)malloc(10 * sizeof(char*));
+  free(x);
+  return x[5];
 }
